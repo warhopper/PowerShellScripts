7ab710ec9d1d93a554a79d582fa764f4d258e477
Colton <coltonaustin@gmail.com>
2021-10-17 15:32:51 -0500

    Update GetUsersNotTaggedByExchange.ps1

--- GetUsersNotTaggedByExchange.ps1
+++ GetUsersNotTaggedByExchange.ps1
@@ -1,3 +1,5 @@
+#Found this useful when trying to troubleshoot a hybrid OnPrem/365 environment and finding users that did not have OnPrem Exchange Attributes. 
+#Dynamic Distribution Groups don't work correctly in that setup, very specific, but very painful
 #Replace the below line with the entire domain or OU you want to search
 $OUpath = 'ou=Anything,DC=Internal,DC=Contoso,DC=local'
 
